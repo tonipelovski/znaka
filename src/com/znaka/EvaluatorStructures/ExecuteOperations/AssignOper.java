65f133f825a3fddb31323ca3e642210c3aced5fa
TheShadow88 <stef4oben88@gmail.com>
2019-05-01 10:52:44 +0300

    Simplified Assign

--- src/com/znaka/EvaluatorStructures/ExecuteOperations/AssignOper.java
+++ src/com/znaka/EvaluatorStructures/ExecuteOperations/AssignOper.java
@@ -2,7 +2,9 @@ package com.znaka.EvaluatorStructures.ExecuteOperations;
 
 import com.znaka.Evaluator;
 import com.znaka.EvaluatorStructures.DataVal;
-import com.znaka.EvaluatorStructures.ExecuteOperations.TypeConversionOperations.*;
+import com.znaka.EvaluatorStructures.ExecuteOperations.TypeConversionOperations.BasicOperation;
+import com.znaka.EvaluatorStructures.ExecuteOperations.TypeConversionOperations.DoubleOper;
+import com.znaka.EvaluatorStructures.ExecuteOperations.TypeConversionOperations.FloatOper;
 import com.znaka.EvaluatorStructures.Variable;
 import com.znaka.Exceptions.CannotEvaluate;
 import com.znaka.Exceptions.UnknownVariable;
@@ -22,24 +24,22 @@ public class AssignOper extends BaseExecuteOper {
     public AssignOper(Evaluator eval) {
         super(AssignAST.class, eval);
         ls = new ArrayList<>();
-        ls.add(new IntOper());
+//        ls.add(new IntOper());
         ls.add(new DoubleOper());
         ls.add(new FloatOper());
-        ls.add(new CharOper());
-        ls.add(new StringOper());
-        ls.add(new BoolOper());
+//        ls.add(new CharOper());
+//        ls.add(new StringOper());
+//        ls.add(new BoolOper());
     }
 
     @Override
     public DataVal exec(DefaultAST ast) throws CannotEvaluate, UnknownVariable, WrongType {
         DataVal ret = null;
-        //System.out.println("Executing assign operation...");
         AssignAST ast1 = (AssignAST)ast;
-        //System.out.println("Operator: " + ast1.getOperator());
-        //System.out.println("Left side: " + ast1.getLeft());
-        //System.out.println("Right side: " + ast1.getRight());
         DataVal rightSide = getEvaluator().Eval(ast1.getRight());
-
+        // create variable if not defined before
+        // evaluate right side
+        // assign right side to variable
 
         final String rightType = rightSide.getType();
         final String rightVal = rightSide.toString();
@@ -49,29 +49,23 @@ public class AssignOper extends BaseExecuteOper {
         final HashSet<Variable> vars = getEvaluator().getVariables();
 
 
-
-
-//            rightType = rightSide.getType();
-            //ret = new DataVal<>(Float.parseFloat(ast1.getRight().getText()));
-//            double nm = Double.parseDouble(rightVal);
-        ret = evalRightSide(leftType, rightSide);
+        ret = applyTypeToRightSide(leftType, rightSide);
 
         if(ret == null){
-            throw new CannotEvaluate("Cannot evaluate right side");
+            //throw new CannotEvaluate("Cannot evaluate right side");
+            ret = rightSide;
         }
 
         Variable var = new Variable<>(varName, ret, false);
         vars.remove(var);
         vars.add(var);
 
-        // create variable if not defined before
-        // evaluate right side
-        // assign right side to variable
+
 
         return ret; // return right side
     }
 
-    private DataVal evalRightSide(String type, DataVal right){
+    private DataVal applyTypeToRightSide(String type, DataVal right){
         for (BasicOperation operation : ls) {
             if(operation.getType().equals(type)){
                 return operation.convert(right);
