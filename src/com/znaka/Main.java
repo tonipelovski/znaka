fd32702e39dfe7eeb908cc07d93c34e83805b3f3
tonipelovski <tonipelovski@gmail.com>
2019-05-15 11:25:15 +0300

    Added assign calculations

--- src/com/znaka/Main.java
+++ src/com/znaka/Main.java
@@ -10,39 +10,44 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) throws IOException, LexerException, ParserException, EvaluatorException {
 	// write your code here
-        URL url = Main.class.getResource("ParserPrinting");
+        Evaluator evaluator;
+        Lexer lexer;
+        URL url = Main.class.getResource("stdlib");
         File file = new File(url.getPath());
         BufferedReader reader = new BufferedReader(new FileReader(file));
-        ArrayList<Token> tokens = new ArrayList<>();
-        Lexer lexer = new Lexer(tokens, reader);
-
-        String lexerOutput = "";
-        while (lexer.readLine()) {
-            lexerOutput = lexerOutput.concat(lexer.tokensToString());
-        }
-        System.out.println(lexerOutput);
-        lexer.resetInput(new BufferedReader(new FileReader(file)));
-        System.out.println(lexer.tokensToString());
+        lexer = new Lexer(reader);
         Parser parser = new Parser(lexer);
-        Evaluator evaluator = new Evaluator(parser);
-
+        evaluator = new Evaluator(parser);
+        evaluator.run();
+        reader.close();
+        //String lexerOutput = "";
+        //while (lexer.readLine()) {
+        //   lexerOutput = lexerOutput.concat(lexer.tokensToString());
+        //}
+        //System.out.println(lexerOutput);
+        //System.out.println(lexer.tokensToString());
+        BufferedReader reader1 = Files.newBufferedReader(Paths.get("/home/theking/IdeaProjects/ZnakaInterpreter/src/com/znaka/ParserPrinting"),
+                StandardCharsets.US_ASCII);
+        lexer.resetInput(reader1);
+        evaluator.run();
         //evaluator.ProcessLine();
        // evaluator.ProcessLine();
-       // evaluator.ProcessLine();
 
 
         //evaluator.ProcessLine();
         //evaluator.ProcessLine();
+        //while(parser.parseLine()){
 
-        while(parser.parseLine()){
-
-        }
+        //}
         System.out.println(parser.toString());
     }
 }
